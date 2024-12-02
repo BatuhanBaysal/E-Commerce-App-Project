fef8713f24f70a5685d4318009aad78519088dc0
Batuhan Baysal <batuhanbaysal3@gmail.com>
2024-12-02 23:08:35 +0300

    Create Repository

---
+++ ecommerce/src/main/java/com/batuhan/ecommerce/repository/BrandRepository.java
@@ -0,0 +1,10 @@
+package com.batuhan.ecommerce.repository;
+
+import com.batuhan.ecommerce.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+
+}
---
+++ ecommerce/src/main/java/com/batuhan/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.batuhan.ecommerce.repository;
+
+import com.batuhan.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+}
