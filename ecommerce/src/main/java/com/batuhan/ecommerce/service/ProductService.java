677a56d3a8582d4d98061e670cd4f5c6ad6b1709
Batuhan Baysal <batuhanbaysal3@gmail.com>
2024-12-03 17:53:43 +0300

    Update ProductService

--- ecommerce/src/main/java/com/batuhan/ecommerce/service/ProductService.java
+++ ecommerce/src/main/java/com/batuhan/ecommerce/service/ProductService.java
@@ -1,10 +1,10 @@
 package com.batuhan.ecommerce.service;
 
 import com.batuhan.ecommerce.model.ProductResponse;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductService {
     ProductResponse getProductById(Integer productId);
-    List<ProductResponse> getAllProducts();
+    Page<ProductResponse> getAllProducts(Pageable pageable, Integer brandId, Integer typeId,String keyword);
 }
