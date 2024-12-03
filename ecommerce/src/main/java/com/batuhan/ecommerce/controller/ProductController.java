83dadac2e136a1bcd6e1ca2da178fca57fe2166c
Batuhan Baysal <batuhanbaysal3@gmail.com>
2024-12-03 17:52:25 +0300

    Update ProductController

--- ecommerce/src/main/java/com/batuhan/ecommerce/controller/ProductController.java
+++ ecommerce/src/main/java/com/batuhan/ecommerce/controller/ProductController.java
@@ -6,12 +6,13 @@ import com.batuhan.ecommerce.model.TypeResponse;
 import com.batuhan.ecommerce.service.BrandService;
 import com.batuhan.ecommerce.service.ProductService;
 import com.batuhan.ecommerce.service.TypeService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,18 +36,29 @@ public class ProductController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<ProductResponse>> getProducts() {
-        List<ProductResponse> allProducts = productService.getAllProducts();
+    public ResponseEntity<Page<ProductResponse>> getProducts(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size,
+            @RequestParam(name = "keyword", required = false) String keyword,
+            @RequestParam(name = "brandId", required = false) Integer brandId,
+            @RequestParam(name = "typeId", required = false) Integer typeId,
+            @RequestParam(name = "sort", defaultValue = "name") String sort,
+            @RequestParam(name = "order", defaultValue = "asc") String order
+    ) {
+        Sort.Direction direction = order.equalsIgnoreCase("desc") ? Sort.Direction.DESC: Sort.Direction.ASC;
+        Sort sorting = Sort.by(direction, sort);
+        Pageable pageable = PageRequest.of(page, size, sorting);
+        Page<ProductResponse> allProducts = productService.getAllProducts(pageable, brandId, typeId, keyword);
         return new ResponseEntity<>(allProducts, HttpStatus.OK);
     }
 
-    @GetMapping("brands")
+    @GetMapping("/brands")
     public ResponseEntity<List<BrandResponse>> getBrands() {
         List<BrandResponse> allBrands = brandService.getAllBrands();
         return new ResponseEntity<>(allBrands, HttpStatus.OK);
     }
 
-    @GetMapping("types")
+    @GetMapping("/types")
     public ResponseEntity<List<TypeResponse>> getTypes() {
         List<TypeResponse> allTypes = typeService.getAllTypes();
         return new ResponseEntity<>(allTypes, HttpStatus.OK);
