bf7f4ad65e71312bda5c6d69a91386925ccb2287
Batuhan Baysal <batuhanbaysal3@gmail.com>
2024-12-15 12:07:32 +0300

    Update Router.tsx

--- ecommerce/client/src/app/router/Router.tsx
+++ ecommerce/client/src/app/router/Router.tsx
@@ -6,16 +6,31 @@ import ContactPage from "../../features/contact/ContactPage";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import NotFound from "../errors/NotFoundError";
 import ServerError from "../errors/ServerError";
+import BasketPage from "../../features/basket/BasketPage";
+import SignInPage from "../../features/account/SignInPage";
+import RegisterPage from "../../features/account/RegisterPage";
+import CheckoutPage from "../../features/checkout/CheckoutPage";
+import RequireAuth from "./RequireAuth";
+import Order from "../../features/orders/Order";
 
 export const router = createBrowserRouter([
     {
         path:'/',
         element: <App/>,
         children:[
+            {
+                element: <RequireAuth/>, children:[
+                    {path:'checkout', element:<CheckoutPage/>},
+                    {path:'orders', element:<Order/>},
+                ]
+            },
             {path: '', element:<HomePage/>},
             {path: 'store', element:<Catalog/>},
             {path: 'store/:id', element:<ProductDetails/>},
             {path: 'contact', element:<ContactPage/>},
+            {path: 'basket', element:<BasketPage/>},
+            {path: 'login', element:<SignInPage/>},
+            {path: 'register', element:<RegisterPage/>},
             {path: 'not-found', element:<NotFound/>},
             {path: 'server-error', element:<ServerError/>},
             {path: '*', element:<Navigate replace to='/not-found'/>}
