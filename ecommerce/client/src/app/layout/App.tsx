c89e2d71229d07d299c6fec314d057b00afb05fc
Batuhan Baysal <batuhanbaysal3@gmail.com>
2024-12-15 12:05:48 +0300

    Update Layout

--- ecommerce/client/src/app/layout/App.tsx
+++ ecommerce/client/src/app/layout/App.tsx
@@ -1,13 +1,35 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import Header from "./Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { fetchCurrentUser } from "../../features/account/accountSlice";
+import { setBasket } from "../../features/basket/basketSlice";
+import agent from "../api/agent";
+import { useAppDispatch } from "../store/configureStore";
+import Spinner from "./Spinner";
+import { getBasketFromLocalStorage } from "../util/util";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light';
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=>{
+    const basket = getBasketFromLocalStorage();
+    dispatch(fetchCurrentUser());
+    if(basket){
+      agent.Baskets.get()
+      .then(basket=>dispatch(setBasket(basket)))
+      .catch(error=>console.log(error))
+      .finally(()=>setLoading(false))
+    }else{
+      setLoading(false);
+    }
+  })
+
   const theme = createTheme({
     palette:{
       mode:paletteType,
@@ -16,16 +38,16 @@ function App() {
   function handleThemeChange(){
     setDarkMode(!darkMode);
   }
-
+  if(loading)return <Spinner message="Getting Basket..."/>
   return (
-      <ThemeProvider theme={theme}>
-        <ToastContainer position="bottom-right" hideProgressBar theme="colored"/>
-      <CssBaseline/>  
-      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
-      <Container sx={{ paddingTop: "64px"}}>
-        <Outlet/>
-      </Container>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored"/>
+    <CssBaseline/>
+    <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+    <Container sx={{ paddingTop: "64px" }}>
+      <Outlet/>
+    </Container>
+    </ThemeProvider>
   )
 }
 
